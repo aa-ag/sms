dcbbf6d1538da2142f7556cb12e6c161ae65421a
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-16 07:38:51 -0600

    refactored to make more readable, added .gitignore, updated text.txt

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+############------------ IMPORTS ------------############
 import os
 import config
 
+############------------ GLOBAL VARIABLE(S) ------------############
+recipient = config.NUM
+
+
+############------------ FUNCTION(S) ------------############
 def parse_words(text):
     with open(text, 'r') as text:
         line = text.readlines()[0]
         words = line.split()
     return words
 
-def send_sms(num, msg):
-    os.system(f'osascript send.scpt {config.NUM} "{msg}"')
 
+def send_sms(num, msg):
+    os.system(f'osascript send.scpt {recipient} "{msg}"')
+
+
+############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
     messages = parse_words('text.txt')
     for message in messages:
-        send_sms(config.NUM, message)
+        send_sms(recipient, message)
